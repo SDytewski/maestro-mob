33b04789be06ed354040d5f5acbe3db1adc5df77
Steve Dytewski <dy2ski@gmail.com>
2024-03-06 17:06:57 -0800

    created firebase starter code

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLyuQYkJiwwgtce_Qd2u180x1V9BjvaII",
+  authDomain: "music-mob-5637e.firebaseapp.com",
+  projectId: "music-mob-5637e",
+  storageBucket: "music-mob-5637e.appspot.com",
+  messagingSenderId: "381934294274",
+  appId: "1:381934294274:web:8e53aa56ae1e3364e01e1b",
+  measurementId: "G-7Q6L4ZE3RG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
