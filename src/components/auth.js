ce1902ab2ebd7d416a9e5d3e0dba243c26b819c8
Steve Dytewski <dy2ski@gmail.com>
2024-03-11 18:38:42 -0700

    created email states

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,9 +1,16 @@
 export const Auth = () => {
+
+    const [email, setEmail] = useState("");
+
+const signIn () => {
+
+};
+
     return (
      <div>
         <input placehold="Email.." />
         <input placehold="Password.." />
-        <button>Sign In</button>
+        <button onClick={signIn}>Sign In</button>
     </div>
     );
 };
