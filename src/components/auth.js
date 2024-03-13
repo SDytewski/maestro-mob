04be98b54be561f3b815648a9db00937ddea77a6
Steve Dytewski <dy2ski@gmail.com>
2024-03-12 22:14:54 -0700

    got authorization hooked up too firebase for email and password

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,4 +1,4 @@
-
+import { auth } from "../config/firebase";
 import {createUserWithEmailAndPassword} from "firebase/auth"
 import {useState} from "react";
 
@@ -7,7 +7,9 @@ export const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-const signIn = () => {
+const signIn = async () => {
+    await createUserWithEmailAndPassword(auth, email, password);
+
 
 };
 
