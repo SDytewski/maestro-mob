816c10a24e1dcc9d49ba946edc6547d9b39592a7
Steve Dytewski <dy2ski@gmail.com>
2024-03-12 22:09:33 -0700

    adding email and password states for auth in firebase

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,15 +1,26 @@
+
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import {useState} from "react";
+
 export const Auth = () => {
 
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-const signIn () => {
+const signIn = () => {
 
 };
 
     return (
      <div>
-        <input placehold="Email.." />
-        <input placehold="Password.." />
+        <input 
+        placehold="Email.."
+        onChange={(e) => setEmail(e.target.value)}
+        
+        />
+        <input placehold="Password.."
+        onChange={(e) => setPassword(e.target.value)}
+        />
         <button onClick={signIn}>Sign In</button>
     </div>
     );
