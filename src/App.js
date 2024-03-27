42dcd7ed35cb17d4d47bd3220c14881522e12661
Steve Dytewski <dy2ski@gmail.com>
2024-03-26 19:45:29 -0700

    added firestore database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
-import {Auth} from "./components/auth";
+import { Auth } from "./components/auth";
+import { db } from "./config/firebase";
+import { getDocs, collection } from 'firebase/firestore'
+
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  
+  const moviesCollectionRef = collection(db, "musicians")
+
+  useEffect(() => {
+   const getMovieList = async () => {
+    try {
+      
+        // Read the dAta
+        // Set the movie list
+        const data = await getDocs();
+        console.log(data);
+
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getMovieList();
+  }, []);
+
   return (
     <div className="App">
-     <Auth />
+      <Auth />
     </div>
-   
+
   );
 }
 
