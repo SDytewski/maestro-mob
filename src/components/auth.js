c9732bb3a81146445c53e3e6e255470fe2339851
Steve Dytewski <dy2ski@gmail.com>
2024-07-01 20:23:16 -0700

    added material ui and buttons and grid

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,6 +1,12 @@
 import { auth, googleProvider } from "../config/firebase";
-import {createUserWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth"
-import {useState} from "react";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
+import { useState } from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 export const Auth = () => {
 
@@ -10,57 +16,79 @@ export const Auth = () => {
     console.log(auth?.currentUser?.email);
 
 
-//  console.log(auth?.currentUser.email);   
-const signIn = async () => {
+    //  console.log(auth?.currentUser.email);   
+    const signIn = async () => {
 
-    try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-        console.error(err);
-    }
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err);
+        }
 
-};
-
-
-const signInWithGoogle = async () => {
-
-    try {
-    await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-        console.error(err);
-    }
-
-};
+    };
 
 
-const logout = async () => {
+    const signInWithGoogle = async () => {
 
-    try {
-    await signOut(auth);
-    } catch (err) {
-        console.error(err);
-    }
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (err) {
+            console.error(err);
+        }
 
-};
+    };
+
+
+    const logout = async () => {
+
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err);
+        }
+
+    };
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
 
 
     return (
-     <div>
-        <input 
-        placehold="Email.."
-        onChange={(e) => setEmail(e.target.value)}
-        
-        />
-        <input placehold="Password.."
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={signIn}>Sign In</button>
-   
-        <button onClick={signInWithGoogle}>Sign In with Google</button>
-        <button onClick={logout}>LogOut</button>
-   
-    </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <input
+                        placehold="Email.."
+                        onChange={(e) => setEmail(e.target.value)}
+
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <input placehold="Password.."
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button variant="contained" onClick={signIn}>Sign In</Button>
+                </Grid>
+
+                <Grid item xs={2}>
+                    <Button variant="contained" onClick={signInWithGoogle}>Sign In with Google</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button variant="contained" onClick={logout}>LogOut</Button>
+                </Grid>
+
+            </Grid>
+        </Box>
     );
+
 };
 
