0f0885d5e76df085515e71911d687f01f44f56b3
Steve Dytewski <dy2ski@gmail.com>
2024-08-13 20:41:39 -0700

    added material ui signup theme

--- src/components/auth.js
+++ src/components/auth.js
@@ -8,6 +8,15 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 
 export const Auth = () => {
@@ -20,6 +29,7 @@ export const Auth = () => {
 
     //  console.log(auth?.currentUser.email);   
     const signIn = async () => {
+        setEmail(" ");
 
         try {
             await createUserWithEmailAndPassword(auth, email, password);
@@ -50,6 +60,21 @@ export const Auth = () => {
         }
 
     };
+    
+
+    function Copyright(props) {
+        return (
+          <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            {/* <Link color="inherit" href="https://mui.com/">
+              Your Website
+            </Link>{' '} */}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        );
+      }
+
 
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -59,15 +84,110 @@ export const Auth = () => {
         color: theme.palette.text.secondary,
     }));
 
+    const defaultTheme = createTheme();
+
 
 
     return (
+
+        <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  {/* <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link> */}
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      
+
+
+
+
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <h1>Music Mob</h1>
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={6}>
                     <div>
                         <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
                     </div>
@@ -79,7 +199,7 @@ export const Auth = () => {
                         /> */}
                     {/* { </Item> */}
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={6}>
                     <div>
                     <TextField id="outlined-basic" label="Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
                     </div>
@@ -91,26 +211,28 @@ export const Auth = () => {
                     {/* {</Item> */}
                 </Grid>
 
-                <Grid item xs={2}>
-                    <Item>
-                        <Button variant="contained" onClick={signIn}>Sign In</Button>
-                    </Item>
+                <Grid item xs={4}>
+                    
+                        <Button variant="contained"  onClick={() =>{ signIn() ; setEmail(' ')}}>Sign In</Button>
+
+                    
                 </Grid>
 
-                <Grid item xs={2}>
-                    <Item>
+                <Grid item xs={4}>
+            
                         <Button variant="contained" onClick={signInWithGoogle}>Sign In with Google</Button>
-                    </Item>
+                 
                 </Grid>
-                <Grid item xs={2}>
-                    <Item>
+                <Grid item xs={4}>
+                    
                         <Button variant="contained" onClick={logout}>LogOut</Button>
-                    </Item>
+                 
                 </Grid>
 
 
             </Grid>
         </Box>
+        </ThemeProvider>
     );
 
 };
